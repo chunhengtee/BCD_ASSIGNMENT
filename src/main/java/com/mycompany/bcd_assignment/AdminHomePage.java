8599aa75cef2e378dcd77e7efdbf20b9a86083df
shanqian98 <tp055786@mail.apu.edu.my>
2021-11-26 03:06:35 +0800

    GUI

---
+++ src/main/java/com/mycompany/bcd_assignment/AdminHomePage.java
@@ -0,0 +1,26 @@
+package com.mycompany.bcd_assignment;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AdminHomePage extends JFrame{
+    private JFrame frame;
+    private JPanel panel1;
+    private JButton checkAndConfirmOrderButton;
+    private JButton logOutButton;
+
+    public AdminHomePage(){
+        frame = new JFrame("AdminHomePage");
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(300,400));
+        frame.setResizable(false);
+
+        // panel
+        frame.add(panel1);
+
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+    }
+}
--- src/main/java/com/mycompany/bcd_assignment/Main.java
+++ src/main/java/com/mycompany/bcd_assignment/Main.java
@@ -3,7 +3,8 @@ package com.mycompany.bcd_assignment;
 public class Main {
     public static void main(String[] args) {
         //System.out.println("test");
-        new CustomerLogin();
-        new AdminLogin();
+        //new CustomerLogin();
+        //new AdminLogin();
+        new AdminHomePage();
     }
 }
